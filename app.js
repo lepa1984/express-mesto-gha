7e008cfc1ca1e1548d551eaa99b3ff65b7285061
Алексей <62145338+lepa1984@users.noreply.github.com>
2023-06-29 18:01:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ const cardRouter = require('./routes/cards');
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://127.0.0.1/mestodb');
-app.use(express.json());
 
+app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
-    _id: '749c28404372dkdcd664594d',
+    _id: '649d8bf3faf805a03fa9c8d1',
   };
 
   next();
 });
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
-app.use('/*', (req, res) => {
+app.use('*', (req, res, next) => {
   res.status(400).send({ message: 'Запрошен несуществующий роут' });
+  next();
 });
 app.listen(PORT, () => {
   console.log(`Сервер запущен! Порт: ${PORT}`);
