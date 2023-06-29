c1b307267331aca925be91bbc2ff88fdb728297b
Алексей <62145338+lepa1984@users.noreply.github.com>
2023-06-29 23:23:15 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -33,7 +33,7 @@ const getUserById = (req, res) => {
         res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
         return;
       }
-        res.status(200).send(user);
+      res.send(user);
     })
     .catch((error) => {
       if (error.name === 'CastError') {
@@ -51,7 +51,7 @@ const updateUserInfo = (req, res) => {
         res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
         return;
       }
-      res.send(user);
+      res.status(200).send(user);
     })
     .catch((error) => {
       if (error.name === 'ValidationError') {
