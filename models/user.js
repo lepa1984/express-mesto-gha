7105d3cb48b55d71e1e288e32f6699fed9e038bc
Алексей <62145338+lepa1984@users.noreply.github.com>
2023-06-29 23:43:33 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -18,7 +18,4 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
-
-const User = mongoose.model('user', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('user', cardSchema);
