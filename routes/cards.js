e136742f3f30e6b80ffe33ada771353ea838a975
Алексей <62145338+lepa1984@users.noreply.github.com>
2023-06-29 18:02:09 +0300

    Update cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -7,9 +7,9 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/cards', getCards);
-router.post('/cards', createCard);
-router.delete('/cards/:cardId', deleteCard);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+router.get('/', getCards);
+router.post('/', createCard);
+router.delete('/:cardId', deleteCard);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
 module.exports = router;
