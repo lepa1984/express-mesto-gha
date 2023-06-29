06086105057cc0e747579b7467d85b78e0785c1f
Алексей <62145338+lepa1984@users.noreply.github.com>
2023-06-29 21:38:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
 mongoose.connect('mongodb://127.0.0.1/mestodb');
 const userRouter = require('./routes/users');
