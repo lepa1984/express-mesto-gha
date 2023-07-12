ee7b3bbcab774eaa6a246a9eb601fd70ed55c340
Alexey Popov <lepa1984@mail.ru>
2023-07-12 22:18:21 +0300

    sprint 14, 1

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,11 +1,12 @@
 const { INTERNAL_SERVER_ERROR } = require('../utils/constants');
-module.exports = (err, req, res, next) => {
+
+const errorHandler = (err, req, res, next) => {
   if (err.statusCode) {
     res.status(err.statusCode).send({ message: err.message });
   } else {
-    res
-      .status(INTERNAL_SERVER_ERROR)
-      .send({ message: err.message || 'Сервер не отвечает' });
+    res.status(INTERNAL_SERVER_ERROR).send({ message: 'Сервер не отвечает' });
   }
   next();
 };
+
+module.exports = errorHandler;
