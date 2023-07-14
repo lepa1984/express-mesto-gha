0a34bb9c770400070ec37848bfc3484594cb3d79
Alexey Popov <lepa1984@mail.ru>
2023-07-14 11:46:35 +0300

    sprint 14, 1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,12 +3,10 @@ const AuthError = require('../errors/AuthError');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  // Там токен
-  const { authorization } = req.headers;
-  const token = authorization.replace('Bearer ', '');
+  const jwtToken = req.cookies.token;
   let payload;
   try {
-    payload = jwt.verify(token, 'unique-secret-key');
+    payload = jwt.verify(jwtToken, 'unique-secret-key');
   } catch (err) {
     return next(new AuthError('Необходима авторизация'));
   }
